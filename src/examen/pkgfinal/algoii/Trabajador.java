23a884f6e32cdae98dad04b80c8ba50c778db116
JesusCondor16 <jesus.condor3@unmsm.edu.pe>
2023-07-14 18:32:59 -0500

    Creacion de clases y atrbituso de Trabajador

---
+++ src/examen/pkgfinal/algoii/Trabajador.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen.pkgfinal.algoii;
+
+/**
+ *
+ * @author user
+ */
+public class Trabajador {
+    private String password;
+    private String email;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
---
+++ src/examen/pkgfinal/algoii/app.java
@@ -0,0 +1,17 @@
+
+package examen.pkgfinal.algoii;
+
+/**
+ *
+ * @author user
+ */
+public class app {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
